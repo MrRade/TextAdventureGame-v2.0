ca8b05297b111747d1eac0560e759fe9c1769717
MAXSCHOL <max.scholz@ottogroup.com>
2022-04-01 11:06:03 +0200

    initial commit oder sowas halt

---
+++ src/main/kotlin/de/tm/textadventuregame/TextadventuregameApplication.kt
@@ -0,0 +1,11 @@
+package de.tm.textadventuregame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TextadventuregameApplication
+
+fun main(args: Array<String>) {
+    runApplication<TextadventuregameApplication>(*args)
+}
---
+++ src/test/kotlin/de/tm/textadventuregame/TextadventuregameApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.tm.textadventuregame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TextadventuregameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
