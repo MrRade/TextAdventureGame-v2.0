17883418df1376782591a9754023d6cc2a5b7845
TDOERING <Tim.Doering@ottogroup.com>
2022-04-08 14:13:47 +0200

    Dependencies for Spotless/ktLint, jacoco were added, Tests will be run before the build

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+
+    id("com.diffplug.spotless") version "6.4.2"
+    jacoco
 }
 
 group = "de.tm"
@@ -33,4 +36,35 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+val ktLintVersion = "0.39.0"
+spotless {
+    kotlin {
+        ktlint(ktLintVersion)
+        targetExclude("src/app/node_modules/**")
+        target(
+            "src/main/kotlin/**/*.kt",
+            "src/test/kotlin/**/*.kt",
+            "src/main/java/**/*.kt",
+            "src/test/java/**/*.kt"
+        )
+    }
+    kotlinGradle {
+        ktlint(ktLintVersion)
+        targetExclude("node_modules/**")
+        targetExclude("build/**")
+        target("build.gradle.kts")
+    }
+}
+
+tasks.named("build") {
+    dependsOn("spotlessCheck", "jacocoTestReport", "test")
 }
--- backend/src/test/kotlin/de/tm/textadventuregame/TextadventuregameApplicationTests.kt
+++ backend/src/test/kotlin/de/tm/textadventuregame/TextadventuregameApplicationTests.kt
@@ -9,5 +9,4 @@ class TextadventuregameApplicationTests {
     @Test
     fun contextLoads() {
     }
-
 }
