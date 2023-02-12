b234c489718a4e20779d65d7b506eefa430cd875
MrRade <72949486+MrRade@users.noreply.github.com>
2023-02-12 22:54:00 +0100

    maked new LanguageButton.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,22 @@ import {HomePage} from "./pages/HomePage";
 import {BrowserRouter} from "react-router-dom";
 import {Route, Routes} from "react-router";
 import {GameListPage} from "./pages/GameListPage";
+import i18n from "./i18n/i18n";
+import {I18nextProvider} from "react-i18next";
 
 function App() {
 
     return (
         <BrowserRouter>
-            <div className="bg-zinc-800 h-screen w-full text-white">
-                <NavBar/>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/gamelist" element={<GameListPage />} />
-                </Routes>
-            </div>
+            <I18nextProvider i18n={i18n}>
+                <div className="bg-zinc-800 h-screen w-full text-white">
+                    <NavBar/>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/gamelist" element={<GameListPage />} />
+                    </Routes>
+                </div>
+            </I18nextProvider>
         </BrowserRouter>
     );
 }
---
+++ frontend/src/i18n/i18n.ts
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        debug: false,
+        saveMissing: true
+    });
+
+export default i18n;
+
+export const getLanguageLabel = (code: string) => {
+    switch (code) {
+        case 'en':
+            return 'EN';
+        case 'de':
+            return 'DE';
+        default:
+            return 'unknown';
+    }
+};
+
+export const getAvailableLanguages = () => ['en', 'de'];
+export const getAvailableOtherLanguages = () => {
+    getAvailableLanguages().filter((l) => i18n.language !== l);
+};
+export const getLanguage = (): string => i18n.language
+    || (typeof window !== 'undefined' && window.localStorage.i18nextLng)
+    || 'en';
