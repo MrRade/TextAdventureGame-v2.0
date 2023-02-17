e8068040215c12d05d6d0d77211ce80cf8b27f94
MrRade <72949486+MrRade@users.noreply.github.com>
2023-02-17 15:16:10 +0100

    fixed translation

--- frontend/src/pages/GameListPage.tsx
+++ frontend/src/pages/GameListPage.tsx
@@ -4,7 +4,7 @@ export const GameListPage = () =>{
     return(
         <div>
             <div className="white text-black w-72 h-72">
-
+                Hallo ich bin eine Liste.
             </div>
         </div>
     )
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import {useTranslation} from "react-i18next";
 
 export const HomePage = () =>{
     const navigate = useNavigate()
-    const t = useTranslation
+    const {t} = useTranslation()
 
     return(
         <div className="text-center ">
@@ -12,7 +12,7 @@ export const HomePage = () =>{
             onClick={() =>navigate("/gamelist")}
             className="border-solid border-white border-2 bg-zinc-700 rounded m-2 p-0.5 hover:bg-zinc-500 shadow-md shadow-gray-500"
             >
-                List of Games
+                {t("button")}
             </button>
         </div>
     )
