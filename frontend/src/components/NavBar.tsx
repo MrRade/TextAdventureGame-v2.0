5920eafb2fbe1b57ea9e3a8ff24c5b5588f912be
MrRade <72949486+MrRade@users.noreply.github.com>
2023-02-22 23:17:17 +0100

    link h2 with ref to homepage

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { LanguageButton } from '../i18n/LanguageButton';
+import {LanguageButton} from '../i18n/LanguageButton';
+import {useNavigate} from "react-router";
 
 export function NavBar() {
-  return (
-    <div className="px-2 sm:px-6 lg:px-8 bg-zinc-900 text-white sticky top-0">
-      <div className="flex h-16 items-center justify-between">
-        <h2 className="font-bold">Textadventuregame v2</h2>
-        <LanguageButton />
-      </div>
-    </div>
-
-  );
+    const navigate = useNavigate()
+    return (
+        <div className="px-2 sm:px-6 lg:px-8 bg-zinc-900 text-white sticky top-0">
+            <div className="flex h-16 items-center justify-between">
+                <h2 className="font-bold cursor-pointer" onClick={() => navigate('/')}>Textadventuregame v2</h2>
+                <LanguageButton/>
+            </div>
+        </div>
+    );
 }
